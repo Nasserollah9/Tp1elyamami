db86363c6f4010bb54acf933ab33d6bef5e6195c
Nasserollah9 <nasro.elyamami@gmail.com>
2025-10-29 10:19:26 +0100

    creation de package llm

---
+++ src/main/java/ma/emsi/elyamami/tp1elyamami/Llm/LlmInteraction.java
@@ -0,0 +1,11 @@
+package ma.emsi.elyamami.tp1elyamami.Llm;
+
+public record LlmInteraction(
+        String questionJson,
+        String questionExtraite,
+        String ReponseJson
+
+) {
+
+        }
+
